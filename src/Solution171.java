960062fc47fc03b6f78eaf3812e4c38e8f8a195f
333caowei <333caowei@sina.com>
2016-05-18 13:50:43 +0900

    Solution171

---
+++ src/Solution171.java
@@ -0,0 +1,24 @@
+/**
+ * Created by caowei on 16/5/18.
+ */
+public class Solution171 {
+
+    public static void main(String args[]){
+        Solution171 solution171 = new Solution171();
+
+        String s = "AAA";
+        int column = solution171.titleToNumber(s);
+        System.out.print(column);
+    }
+
+    
+    public int titleToNumber(String s) {
+        char alphabet[] = s.toCharArray();
+        int sum=0;
+        for(int i=0;i<alphabet.length;i++){
+            sum+=Math.pow(26,alphabet.length-i-1)*(alphabet[i]-'A'+1);
+        }
+
+        return sum;
+    }
+}
